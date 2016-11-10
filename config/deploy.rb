d6b68de176957ad837614c544e20be4925fb0227
chenlong <KLoong777@163.com>
2016-11-10 13:55:35 +0800

    add s

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,47 @@
 # config valid only for current version of Capistrano
-lock '3.4.1'
+lock '3.6.1'
 
 set :application, 'story_house'
 set :scm, :git
 set :repo_url, 'git@github.com:KLoong/story_house.git'
+set :branch, 'master'
 
-
+set :log_level, :debug
 # rbenv
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :branch, 'master'
+
 # how many old releases do we want to keep, not much
 set :keep_releases, 5
 
 
 # files we want symlinking to specific entries in shared
-set :linked_files, %w{config/database.yml config/application.rb config/secrets.yml}
+# set :linked_files, %w{config/database.yml config/application.rb config/secrets.yml}
 
 # dirs we want symlinking to shared
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+namespace :deploy do
+  #ENV['BUNDLE_GEMFILE'] = "#{deploy_to}/current/Gemfile"
+  desc 'Restart application'
+    task :restart do
+      invoke 'unicorn:stop'
+      sleep 2
+      invoke 'unicorn:start'
+    end
 
+  task :stop do
+    invoke 'unicorn:stop'
+  end
+
+  desc 'Start application'
+  task :start do
+    invoke 'unicorn:start'
+  end
+
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -53,15 +72,56 @@ set :linked_files, %w{config/database.yml config/application.rb config/secrets.y
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+# namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
 
-end
+# end
+
+
+# config valid only for Capistrano 3.1
+# lock '3.2.1'
+
+# set :application, 'yamaha_music_school'
+# set :scm, :git
+# set :repo_url, 'ssh://git@114.80.200.91:10022/home/git/repos/yamaha_music_school.git '
+# set :branch, 'master'
+
+# set :log_level, :debug
+# # set :asset_env, "RAILS_GROUPS=assets"
+
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets public/uploads public/minisite/music_school/uploads  config/unicorn  }
+# set :linked_files, %w{ config/database.yml }
+    
+# set :rbenv_type, :user         
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+# set :bundle_flags, "--deployment --binstubs"
+
+
+# namespace :deploy do
+#   #ENV['BUNDLE_GEMFILE'] = "#{deploy_to}/current/Gemfile"
+#   desc 'Restart application'
+#     task :restart do
+#       invoke 'unicorn:stop'
+#       sleep 2
+#       invoke 'unicorn:start'
+#     end
+
+#   task :stop do
+#     invoke 'unicorn:stop'
+#   end
+
+#   desc 'Start application'
+#   task :start do
+#     invoke 'unicorn:start'
+#   end
+
+# end
+# set :default_env, { rvm_bin_path: '~/.rvm/bin' }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,24 +8,25 @@ set :branch, 'master'
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 server '139.196.210.99', user: 'root', roles: %w{web app db} 
-server '139.196.210.99',
-  user: 'root',
-  roles: %w{web app db},
-  ssh_options: {
-    user: 'root', # overrides user setting above
-    keys: %w(/home/user_name/.ssh/id_rsa),
-    forward_agent: false,
-    auth_methods: %w(publickey password)
-    # password: 'please use keys'
-  }
+# server '139.196.210.99',
+#   user: 'root',
+#   roles: %w{web app db},
+#   ssh_options: {
+#     user: 'root', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
 
 
 
 
 set :deploy_to, "/root/projects/story_house/production"
 
-set :rails_env, :production
-
+set :rails_env, 'production'
+set :migration_role, 'db'
+set :unicorn_port, 3000   
 # whether we're using ssl or not, used for building nginx
 # config file
 set :enable_ssl, false
@@ -43,6 +44,17 @@ set :unicorn_worker_count, 5
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
+# set :rbenv_ruby, '2.1.2'
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :deploy_to, '/var/www/yamaha_music_school/staging'
+# set :stage, :staging
+# set :rails_env, 'production'   
+# set :migration_role, 'db'      
+# set :unicorn_port, 2222
+# set :assets_prefix, "minisite/assets"
+# #set :branch, 'staging'
+# # server '222.73.93.158', user: 'kembo', roles: %w{web app db},port: 10022
+# server 'web2.yamaha.com.cn', user: 'app_user', roles: %w{web app db},port: 10022
 
 
 # Configuration
